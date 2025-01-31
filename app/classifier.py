9403d833c6ed410cafd11cc6abd92dc2a4c81586
Gabriel Luchi <105859517+galuchi@users.noreply.github.com>
2025-01-31 04:38:29 -0300

    Update classifier.py

--- app/classifier.py
+++ app/classifier.py
@@ -5,10 +5,11 @@ import os
 client = Groq(api_key="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")
 
 
-#Classifica o texto como 'Produtivo' ou 'Improdutivo' e gera uma resposta automática.
 
+# Função para classificar um email como 'Produtivo' ou 'Improdutivo' e gerar uma resposta automática.
 def classify_and_generate_response(text):
    
+    #Prompt fornecido para a API IA
     prompt = f"""
     Você é um assistente que classifica emails como 'Produtivo' ou 'Improdutivo' e gera uma resposta automática.
     
@@ -26,20 +27,25 @@ def classify_and_generate_response(text):
     Se for Produtivo, gere uma resposta curta e direta.
     """
 
+    #Chama a API para fornecer a resposta
     response = client.chat.completions.create(
-        model="llama-3.2-3b-preview",
+        model="llama-3.2-3b-preview", #Modelo da IA
         messages=[{"role": "user", "content": prompt}],
         temperature=0.7,
         max_tokens=150
     )
 
+    # Exibe a resposta bruta da IA no console para depuração.
     print("Resposta bruta da IA:", response.choices[0].message.content)
+
+    # Divide a resposta da IA em linhas para facilitar a extração dos dados.
     output = response.choices[0].message.content.strip().split("\n")
 
-     # Garantir que temos pelo menos duas linhas válidas
+    # Garantir que temos pelo menos duas linhas válidas
     if len(output) < 2:
         return {"category": output[0].split(":")[-1].strip(), "response": "Erro: resposta não gerada pela IA."}
 
+    # Extração da categoria e resposta a partir do texto formatado.
     category = output[0].split(":")[-1].strip()
     generated_response = output[1].split(":")[-1].strip()
 
