a98a40df718152c72c2357b7138e2526328a8a74
Gabriel Luchi <105859517+galuchi@users.noreply.github.com>
2025-01-31 04:54:38 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 import os
 from app import create_app
 
+# Cria a instância do aplicativo Flask
 app = create_app()
 
+#Inicia o servidor Flask, definindo a porta automaticamente com base na variável de ambiente.
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Railway define a variável PORT automaticamente
-    app.run(host="0.0.0.0", port=port)
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)  # Executa o app Flask na porta definida
